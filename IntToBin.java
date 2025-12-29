f0458de4e7dbe3a556766a54a7a45d7178b10f9e
Danielle Sinay <daniellesinay@Danielles-MacBook-Pro.local>
2025-12-29 17:25:13 +0200

    IntToBin.1

--- IntToBin.java
+++ IntToBin.java
@@ -8,6 +8,7 @@ public class IntToBin {
 
     /** Returns the binary representation of the given integer, as a string of 0's and 1's.  */
     public static String toBinary(int x) {
+        // base cases: 1, 0 (both binary)
         if (x == 0) {
             return "0";
         }
@@ -16,6 +17,8 @@ public class IntToBin {
             return "1";
         }
 
+        // divide the int by 2 to remove the last binary digit 
+        // add last binary digit (1 or 0) to the binary String
         return toBinary(x / 2) + (x % 2);
     }    
  }
