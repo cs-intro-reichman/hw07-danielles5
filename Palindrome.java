85ce412a2b10ee13ee236eb5d19a12ccafe411c1
Danielle Sinay <daniellesinay@Danielles-MacBook-Pro.local>
2025-12-29 17:36:23 +0200

    Palindrome.1

--- Palindrome.java
+++ Palindrome.java
@@ -7,7 +7,18 @@ public class Palindrome {
 	
 	/** Checks if the given string is a palindrome. */
 	public static boolean isPalindrome(String s) {
-		//// Replace the followaing statement with your code
-		return false;
+		String lowerS = s.toLowerCase();
+
+		// if the String is one letter or less, automatically a palindrome
+		if (s.length() <= 1) {
+			return true;
+		}
+
+		// if the first letter isn't the same as the last one, automatically not a palindrome
+		if (lowerS.charAt(0) != lowerS.charAt(s.length() - 1)) {
+			return false;
+		}
+
+		return isPalindrome(s.substring(1, s.length() - 1));
     }
 }
